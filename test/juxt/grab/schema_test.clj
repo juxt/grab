5ea2d0601af1e30923253c0aa5dd1989432d99fa
Malcolm Sparks <mal@juxt.pro>
2021-07-30 11:35:54 +0100

    Add directive parsing

--- test/juxt/grab/schema_test.clj
+++ test/juxt/grab/schema_test.clj
@@ -5,12 +5,20 @@
    [clojure.test :refer [deftest is are testing]]
    [juxt.grab.alpha.graphql :as grab]))
 
-(grab/parse-graphql
-   "type Person {
+(def example-56 "
+type Person @crux(query: \"{:find [e] :where [[?e :name][?e :picture ?p][?p :size $size]]}\") {
   name: String
-  picture(size: Int): Url}
+  picture(size: Int): Url}")
 
+(grab/parse-graphql example-56)
+
+(grab/parse-graphql
+ (str
+  example-56
+  "
 schema { query: Person }
 
 query { person }
-")
+
+
+"))
