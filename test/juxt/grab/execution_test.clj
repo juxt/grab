879611fb97b3e3454d85db737c8270967eadf259
Malcolm Sparks <mal@juxt.pro>
2021-08-07 11:05:35 +0100

    Refactor to form test

--- test/juxt/grab/execution_test.clj
+++ test/juxt/grab/execution_test.clj
@@ -2,8 +2,7 @@
 
 (ns juxt.grab.execution-test
   (:require
-   [clojure.test :refer [deftest is are testing]]
-   [clojure.java.io :as io]
+   [clojure.test :refer [deftest is]]
    [juxt.grab.alpha.execution :as execution]
    [juxt.grab.alpha.document :refer [->document]]
    [juxt.grab.alpha.schema :refer [->schema]]))
@@ -12,85 +11,14 @@
 
 (set! *print-level* 20)
 
-(deftest badly-formed-trailing-text
+;; Just to get warmed up
+(deftest warm-up-test
   (is
-   (thrown?
-    clojure.lang.ExceptionInfo
-    (parser/parse-graphql "query foo { user } query bar { }"))
-   "The final query clause is invalid and this should cause an error"))
+   (= {:data {"user" "mal"}, :errors []}
+      (execution/execute-request
+       {:schema (->schema "schema { query: User } type User { user: String }")
+        :document (->document "query { user }")
+        :field-resolver
+        (fn [{:keys [field-name]}] (case field-name "user" "mal"))}))))
 
-#_(deftest query-test
-  (is (= {"users"
-          [{"username" "mal", "email" "mal@juxt.pro"}
-           {"username" "jdt", "email" "jdt@juxt.pro"}
-           {"username" "tim", "email" "tim@juxt.pro"}]}
-
-         (let [document
-               (->  "query { users { username email } }"
-                    #_"{ user }"
-                    grab/parse-graphql
-                    grab/validate-graphql-document)]
-
-           (grab/execute-request
-            {:schema
-             (-> (slurp (io/resource "juxt/grab/test.graphql"))
-                 grab/parse-graphql
-                 grab/validate-graphql-document
-                 schema/document->schema)
-
-             :document document
-
-             :variable-values {}
-
-             :initial-value {"users"
-                             [{"username" "mal" "email" "mal@juxt.pro"}
-                              {"username" "jdt" "email" "jdt@juxt.pro"}
-                              {"username" "tim" "email" "tim@juxt.pro"}]}
-
-             :field-resolver
-             (fn [{:keys [object-type object-value field-name argument-values] :as field}]
-               (cond
-
-                 (= field-name "user")
-                 "Malcolm"
-
-                 (= field-name "users")
-                 (get object-value "users")
-
-                 (= field-name "username")
-                 (get object-value "username")
-
-                 (= field-name "email")
-                 (get object-value "email")
-
-                 :else
-                 (throw
-                  (ex-info
-                   "TODO: Resolve field"
-                   {:field field}))))})))))
-
-
-#_(-> "schema { query: User } type User { user: String }"
-          parser/parse-graphql
-          schema/parse-tree->schema)
-
-#_(-> "query Foo { user }"
-          parser/parse-graphql
-          document/parse-tree->document)
-
-
-(execution/execute-request
- {:schema (->schema "schema { query: User } type User { user: String }")
-  :document (->document "query { user }")
-  :field-resolver
-  (fn [{:keys [object-type object-value field-name argument-values] :as field}]
-    (case field-name
-      "user" "mal"
-      (throw
-       (ex-info
-        "TODO: Resolve field"
-        {:object-type object-type
-         :object-value object-value
-         :field-name field-name
-         :field field})))
-    )})
+;; Example 3
