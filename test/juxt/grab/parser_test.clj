9dd5babaebf3aefe47d87e22f2edbd3569b5921b
Malcolm Sparks <mal@juxt.pro>
2021-08-10 18:51:22 +0100

    Fix test

--- test/juxt/grab/parser_test.clj
+++ test/juxt/grab/parser_test.clj
@@ -8,7 +8,11 @@
 (alias 'reap (create-ns 'juxt.reap.alpha.graphql))
 
 (deftest parse-query-test
-  (is (= '({::reap/operation-type "query",
-            ::reap/selection-set [{::reap/field {::reap/name "user", ::reap/arguments {}}}]})
+  (is (= '({::reap/type "OperationDefinition"
+            ::reap/operation-type "query"
+            ::reap/selection-set
+            [{::reap/selection-type :field
+              ::reap/name "user"
+              ::reap/arguments {}}]})
          (-> "query { user }"
              parser/parse-graphql))))
